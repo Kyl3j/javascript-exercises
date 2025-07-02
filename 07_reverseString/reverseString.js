ffd2c719236b35780440ce419cdc0546cf7b6774
Kyl3j <seun01jay@gmail.com>
2025-07-02 07:37:01 -0500

    finished exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,6 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // Convert the string to an array, reverse it, and join it back to a string
+    return word.split('').reverse().join('');
 
 };
 
