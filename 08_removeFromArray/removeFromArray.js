4395492d06cb225315750a41069c22accd7da5b5
Kyl3j <seun01jay@gmail.com>
2025-07-04 18:11:56 -0500

    finished exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    const array = args[0];
+    const valuesToRemove = args.slice(1);
+    
+    return array.filter(item => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
