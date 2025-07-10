5d466a67e6cccb598ec476d7189197af2a3e7134
Kyl3j <seun01jay@gmail.com>
2025-07-09 21:40:49 -0500

    exercise 09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min > max) {
+        [min, max] = [max, min];
+    }
 
+    if (min < 0 || max < 0) {
+        return 'ERROR';
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return 'ERROR';
+    }
+
+    let total = 0;
+
+    for (let i = min; i <= max; i++) {
+	    total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll; 
