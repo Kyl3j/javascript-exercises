60368ec45b724f84386774a5bf64aa235e30a944
Kyl3j <seun01jay@gmail.com>
2025-07-23 18:52:15 -0500

    finished exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(index_f) {
+    index_f = parseInt(index_f);
+
+    if (index_f==0){
+        return 0;
+    }
+
+    if (index_f<0){
+        return "OOPS";
+    }
+
+    array_f = [1];
+    num_prev = 0;
+
+    for (i=0; i<index_f; i++){
+        num_current = array_f[i];
+        array_f.push(num_prev+num_current)
+        num_prev = num_current;
+    }
+
+    //console.log(array_f)
+
+    return array_f[(index_f-1)];
 
 };
 
